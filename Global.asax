aa273dde7a1773b586ea8b4933009aab79d81e32
Sagar <shaggy.shelar@gmail.com>
2014-01-17 15:54:44 +0530

    Deleted

--- Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ES.ISSample.Web.MvcApplication" Language="C#" %>
--- app/components/product/productRepository.js
+++
@@ -1,9 +0,0 @@
-﻿angular.module('app').factory('productRepository', function ($http, $q) {
-    return {
-        getAll: function () {
-            var deferred = $q.defer();
-            $http.get('/Product/GetAll').success(deferred.resolve).error(deferred.reject);
-            return deferred.promise;
-        }
-    }
-});
